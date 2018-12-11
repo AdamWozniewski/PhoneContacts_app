2a3c217f38f2f2c385cf72983f28f50641b52fc9
Adam Woźniewski <adam.wozniewski@intive.com>
2018-12-11 17:57:33 +0100

    zaimplementowanie wszystkich fragmentów

--- app/src/main/java/com/adam/phonecontacts_app/AddEditFragment.java
+++ app/src/main/java/com/adam/phonecontacts_app/AddEditFragment.java
@@ -1,4 +1,234 @@
 package com.adam.phonecontacts_app;
 
-public class AddEditFragment {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+
+import com.adam.phonecontacts_app.data.DatabaseDescription;
+
+public class AddEditFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Interfejs z metodą wywołąnia zwrotnego implementowania przez główną aktywnośc
+    public interface AddEditFragmentListener {
+        // Wywołanie gdy kontakt jest zapisywany
+        void onAddEditCompleted(Uri uri);
+    }
+
+    // Pole używane do identyfikacji obiektu loader
+    private static final int CONTACT_LOADER = 0;
+
+    // Pole obiektu implementującego zagnieżdzony interfejs - główna aktywność (MainActivity)
+    private AddEditFragmentListener addEditFragmentListener;
+
+    // Adres URI wybranego kontaktu
+    private Uri contactUri;
+
+    // Dodawanie (true) / edycja (false) kontaktu
+    private boolean addingNewContact = true;
+
+    // Pola graficznego interfejsu użytkownika
+    private TextInputLayout nameTextInputLayout;
+    private TextInputLayout phoneTextInputLayout;
+    private TextInputLayout emailTextInputLayout;
+    private TextInputLayout streetTextInputLayout;
+    private TextInputLayout cityTextInputLayout;
+    private TextInputLayout stateTextInputLayout;
+    private TextInputLayout zipTextInputLayout;
+    private FloatingActionButton saveContactFAB;
+
+    // Uzywany wraz z obiektami Snackbar
+    private CoordinatorLayout coordinatorLayout;
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.addEditFragmentListener = (AddEditFragmentListener) context;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.addEditFragmentListener = null;
+    }
+
+    // Utworzenie widoku obiektu Fragment
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+
+        // Przygotowanie elementów graficznego interfejsu użytkownika
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
+        this.nameTextInputLayout = (TextInputLayout) view.findViewById(R.id.nameTextInputLayout);
+        this.nameTextInputLayout.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                AddEditFragment.this.updateSaveButtonFAB();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        this.phoneTextInputLayout = (TextInputLayout) view.findViewById(R.id.phoneTextInputLayout);
+        this.emailTextInputLayout = (TextInputLayout) view.findViewById(R.id.emailTextInputLayout);
+        this.streetTextInputLayout = (TextInputLayout) view.findViewById(R.id.streetTextInputLayout);
+        this.cityTextInputLayout = (TextInputLayout) view.findViewById(R.id.cityTextInputLayout);
+        this.stateTextInputLayout = (TextInputLayout) view.findViewById(R.id.stateTextInputLayout);
+        this.zipTextInputLayout = (TextInputLayout) view.findViewById(R.id.zipTextInputLayout);
+
+        // Przygotowanie przycisku
+        this.saveContactFAB = (FloatingActionButton) view.findViewById(R.id.saveFloatingActionButton);
+        this.saveContactFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ukrycie klawiatury ekranowej
+                ((InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).hideSoftInputFromInputMethod(getView().getWindowToken(), 0));
+
+                // Zapisanie kontaktu w bazie danych
+                AddEditFragment.this.saveContact();
+            }
+        });
+
+        // Kod używany do wyświetlania obiektów SnackBar
+        this.coordinatorLayout = (CoordinatorLayout) getActivity().findViewById(R.id.coordinatorLayout);
+        Bundle args = getArguments();
+
+        if (args != null) {
+            this.addingNewContact = false;
+            this.contactUri = args.getParcelable(MainActivity.CONTACT_URI);
+        }
+
+        // Utowrzenie obiektu Loader
+        if (this.contactUri != null) {
+            getLoaderManager().initLoader(CONTACT_LOADER, null, this);
+        }
+
+        // Zwrócenie widoku
+        return view;
+
+    }
+
+    // Wyświetla lub chowa przycisk zapisu kontaktu (FAB)
+    private void updateSaveButtonFAB() {
+        // Pobierz tekst z pola EditText
+        String input = this.nameTextInputLayout.getEditText().getText().toString();
+
+        // Wyświtl lub schowaj przycisk zapisu kontaktu
+        if (input.trim().length() != 0) {
+            this.saveContactFAB.show();
+        } else {
+            this.saveContactFAB.hide();
+        }
+    }
+
+    // Zapisuje dane kontaktu w bazie danych
+    private void saveContact() {
+        // Utworzenie i obsadzeineie obiektu content values
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DatabaseDescription.Contact.COLUMN_NAME, this.nameTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_PHONE, this.phoneTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_EMAIL, this.emailTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_STREET, this.streetTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_CITY, this.cityTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_STATE, this.stateTextInputLayout.getEditText().getText().toString());
+        contentValues.put(DatabaseDescription.Contact.COLUMN_ZIP, this.zipTextInputLayout.getEditText().getText().toString());
+
+        // Zapisanie kontaktu w bazie danych korzystając z obiektu AddressBookContentProvider i jego metody insert
+        if (this.addingNewContact) {
+            // Dodanie kontaktu
+            Uri newContactUri = getActivity().getContentResolver().insert(DatabaseDescription.Contact.CONTENT_URI, contentValues);
+
+            // Wyświetlenie obiektoów Snackbar
+            if (newContactUri != null) {
+                Snackbar.make(this.coordinatorLayout, R.string.contact_added, Snackbar.LENGTH_LONG).show();
+                this.addEditFragmentListener.onAddEditCompleted(newContactUri);
+            } else {
+                Snackbar.make(this.coordinatorLayout, R.string.contact_not_added, Snackbar.LENGTH_LONG).show();
+            }
+        }
+
+        // Aktualizacja kontaktu w bazie danych korzystając z obiektu AddressBookContentProvider i jego metody update
+        else {
+            // Aktualizacja kontaktu
+            int updateRows = getActivity().getContentResolver().update(this.contactUri, contentValues, null, null);
+
+            // Wyświetlenie obiektów snackbar
+            Uri ContactUri = getActivity().getContentResolver().insert(DatabaseDescription.Contact.CONTENT_URI, contentValues);
+            if (updateRows > 0) {
+                Snackbar.make(this.coordinatorLayout, R.string.contact_updated, Snackbar.LENGTH_LONG).show();
+                this.addEditFragmentListener.onAddEditCompleted(contactUri);
+            } else {
+                Snackbar.make(this.coordinatorLayout, R.string.contact_not_updated, Snackbar.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        switch (i) {
+            case CONTACT_LOADER:
+                return new CursorLoader(getActivity(), contactUri, null, null, null, null);
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        // Sprawdzenie czy wybrany kontakt istnieje w bazie danych
+        if (cursor != null && cursor.moveToFirst()) {
+            // Odczytanie indeksów kolumn z tabeli
+            int nameIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME);
+            int phoneIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_PHONE);
+            int emailIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_EMAIL);
+            int streetIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_STREET);
+            int cityIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_CITY);
+            int stateIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_STATE);
+            int zipIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_ZIP);
+
+            // Wypełniennie pól editText
+            this.nameTextInputLayout.getEditText().setText(cursor.getString(nameIndex));
+            this.phoneTextInputLayout.getEditText().setText(cursor.getString(phoneIndex));
+            this.emailTextInputLayout.getEditText().setText(cursor.getString(emailIndex));
+            this.streetTextInputLayout.getEditText().setText(cursor.getString(streetIndex));
+            this.cityTextInputLayout.getEditText().setText(cursor.getString(cityIndex));
+            this.stateTextInputLayout.getEditText().setText(cursor.getString(stateIndex));
+            this.zipTextInputLayout.getEditText().setText(cursor.getString(zipIndex));
+
+            // Wywoąlnie metody wyświetlającej przycisk zapisu kontaktu
+            this.updateSaveButtonFAB();
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
--- app/src/main/java/com/adam/phonecontacts_app/DetailFragment.java
+++ app/src/main/java/com/adam/phonecontacts_app/DetailFragment.java
@@ -1,4 +1,179 @@
 package com.adam.phonecontacts_app;
 
-public class DetailFragment {
+import android.app.AlertDialog;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.adam.phonecontacts_app.data.DatabaseDescription;
+
+public class DetailFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // Interfejs z metodą wywołania zwrotnego  implementowaną przez główną aktywność
+
+    public interface DetailFragmentListener {
+
+        // Metoda wywołania w przypadku usuwania kontaktu
+        void onContactDeleted();
+
+        // Przekazanie adresu URI, który ma być edytowany
+        void onEditContact(Uri uri);
+    }
+
+    // Pole uzywane do identyfikacji obiektu Loader
+    private static final int CONTACT_LOADER = 0;
+
+    // Pole obiektu implementującego zagnieżdzony interfejs - główna aktywnośc (MainACtivity)
+    private DetailFragmentListener listener;
+
+    // Adres URI wybranego kontaktu
+    private Uri contactUri;
+
+    // Pola widoków TextView
+    private TextView nameTextView;
+    private TextView phoneTextView;
+    private TextView emailTextView;
+    private TextView streetTextView;
+    private TextView cityTextView;
+    private TextView stateTextView;
+    private TextView zipTextView;
+
+    // Inicjalizacja interfejsu DetailFragmentListener przy dołaczaeniu fragmentu
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (DetailFragmentListener) context;
+    }
+
+    // Usuwanie interfejsu DetailFragmentListener przy dołaczaeniu fragmentu
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.listener = null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        setHasOptionsMenu(true);
+
+        // Uzyskanie obiektu Bundle zawierajacego argimenty
+        Bundle args = getArguments();
+
+        if (args != null) {
+            this.contactUri = args.getParcelable(MainActivity.CONTACT_URI);
+        }
+
+        // Przygotowanie elementów graficznego interfejsu użytkownika
+        View view = inflater.inflate(R.layout.fragment_add_edit, container, false);
+
+        this.nameTextView = (TextView) view.findViewById(R.id.nameTextView);
+        this.emailTextView = (TextView) view.findViewById(R.id.phoneTextView);
+        this.streetTextView = (TextView) view.findViewById(R.id.streetTextView);
+        this.cityTextView = (TextView) view.findViewById(R.id.cityTextView);
+        this.stateTextView = (TextView) view.findViewById(R.id.stateTextView);
+        this.zipTextView = (TextView) view.findViewById(R.id.zipTextView);
+
+        // Załądowanie kontaktu
+        getLoaderManager().initLoader(CONTACT_LOADER, null, this);
+
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_details_menu, menu);
+    }
+
+    // Wyświetla elementy menu fragmentu DetailFragment
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                this.listener.onEditContact(this.contactUri);
+                return true;
+            case R.id.action_delete:
+                this.deleteContact();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Usunięcie kontaktu
+    private void deleteContact() {
+        // Utworzenie obiektu AlertDialog potwierdzającego usunięcie wybranego kontaktu
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle(R.string.confirm_title);
+        builder.setMessage(R.string.confirm_message);
+//        builder.setPositiveButton(R.string.button_delete, onClick(dialog, which) -> { <- poprawka
+//            getActivity().getContentResolver().delete(this.contactUri, null, null);
+//            this.listener.onContactDeleted();
+//        });
+        builder.setNegativeButton(R.string.button_cancel, null);
+
+        // Wyświtlanie zdefiniowanego okna dialogowego
+        builder.show();
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, @Nullable Bundle bundle) {
+        CursorLoader cursorLoader;
+
+        switch (i) {
+            case CONTACT_LOADER:
+                cursorLoader = new CursorLoader(getActivity(), this.contactUri, null, null, null, null);
+                break;
+            default:
+                cursorLoader = null;
+                break;
+        }
+        return cursorLoader;
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        // WYświetlanie danych istniejącego kontaktu
+        if (cursor != null && cursor.moveToFirst()) {
+
+            // Odczytanie indeksów kolumn z tabeli
+            int nameIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_NAME);
+            int phoneIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_PHONE);
+            int emailIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_EMAIL);
+            int streetIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_STREET);
+            int cityIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_CITY);
+            int stateIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_STATE);
+            int zipIndex = cursor.getColumnIndex(DatabaseDescription.Contact.COLUMN_ZIP);
+
+            // Wypełnienie pól EditText
+            this.nameTextView.setText(cursor.getString(nameIndex));
+            this.phoneTextView.setText(cursor.getString(phoneIndex));
+            this.emailTextView.setText(cursor.getString(emailIndex));
+            this.streetTextView.setText(cursor.getString(streetIndex));
+            this.cityTextView.setText(cursor.getString(cityIndex));
+            this.stateTextView.setText(cursor.getString(stateIndex));
+            this.zipTextView.setText(cursor.getString(zipIndex));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+
+    }
 }
